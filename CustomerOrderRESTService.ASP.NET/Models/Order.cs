99d3e68d973d884f9f9faa8e83c3cd181582ed53
Chania Beké <chaniabeke@hotmail.com>
2020-11-22 17:36:25 +0100

    .

--- CustomerOrderRESTService.ASP.NET/Models/Order.cs
+++ CustomerOrderRESTService.ASP.NET/Models/Order.cs
@@ -8,7 +8,6 @@ namespace CustomerOrderRESTService.ASP.NET.Models
 {
     public class Order
     {
-        public int Id { get; set; }
         public int CustomerId { get; set; }
         public int Amount { get; set; }
         public ProductType Product { get; set; }
